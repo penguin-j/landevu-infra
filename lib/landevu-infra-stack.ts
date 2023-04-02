665cfdd5a61a4a6a615d8be3e3d36a721b417b75
j <penguinjinbei@gmail.com>
2023-04-03 01:01:17 +0900

    add: add public subnet

--- lib/landevu-infra-stack.ts
+++ lib/landevu-infra-stack.ts
@@ -6,6 +6,16 @@ export class LandevuInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     const vpc = new ec2.Vpc(this, 'LandevuVpc', {
+      vpcName: "st-landevu-vpc",
+      cidr: "10.0.0.0/16",
+      natGateways: 0,
+      maxAzs: 1,
+      subnetConfiguration: [
+        {
+          name: "st-landevu-public-subnet",
+          subnetType: ec2.SubnetType.PUBLIC
+        }
+      ]
     })
   }
 }
