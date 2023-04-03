19f407410c09015cd2f028113e45b2f302ed888e
j <penguinjinbei@gmail.com>
2023-04-04 02:08:42 +0900

    refactor

--- lib/landevu-infra-stack.ts
+++ lib/landevu-infra-stack.ts
@@ -1,30 +1,34 @@
-import * as cdk from 'aws-cdk-lib';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2'
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2"
+import { Construct } from "constructs";
 
 export class LandevuInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const vpc = new ec2.Vpc(this, 'LandevuVpc', {
+    // VPC
+    const landevuVpc = new ec2.Vpc(this, "LandevuVpc", {
       vpcName: "st-landevu-vpc",
       cidr: "10.0.0.0/16",
       natGateways: 0,
       maxAzs: 2,
       subnetConfiguration: [
+        // Public Subnet
         {
           name: "st-landevu-public-subnet",
           subnetType: ec2.SubnetType.PUBLIC
         }
       ]
     })
-    const securityGroupAlb = new ec2.SecurityGroup(this, "LandevuAlbSg", {
-      vpc: vpc,
+    // Security Group for ALB
+    const landevuAlbSecurityGroup = new ec2.SecurityGroup(this, "LandevuAlbSg", {
+      vpc: landevuVpc,
       securityGroupName: "st-landevu-alb-sg"
     })
+    // ALB
     const alb = new elbv2.ApplicationLoadBalancer(this, "LandevuAlb", {
-      vpc: vpc,
-      securityGroup: securityGroupAlb,
+      vpc: landevuVpc,
+      securityGroup: landevuAlbSecurityGroup,
       internetFacing: true,
       loadBalancerName: "st-landevu-alb"
     })
